097443c99206140071d18c2d8c533ab9c7d1b85d
Brian Sizemore <bpsizemore@gmail.com>
2015-07-23 12:18:58 -0400

    restructed files

---
+++ pages/competitions.php
@@ -0,0 +1,108 @@
+<!DOCTYPE HTML>
+<!--
+	Landed by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Competitions</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<?php include("header.html");?>
+			<!-- Main -->
+				<div id="main" class="wrapper style1">
+					<div class="container">
+						<header class="major">
+							<h2>Competitions</h2>
+							<p></p>
+						</header>
+						<div class="row 150%">
+							<div class="4u 12u$(medium)">
+
+								<!-- Sidebar -->
+									<section id="sidebar">
+										<section>
+											<h2><strong>Competition Information</strong></h2>
+											<a href="http://www.nationalcyberleague.org" class="image fit"><img src="images/ncl.png" alt="" /></a>
+											<h3>National Cyber League</h3>
+											<p>The National Cyber League was founded in May 2011 to provide an ongoing virtual training ground for collegiate students to develop, practice, and validate their cybersecurity skills. Using lab exercises designed around industry-recognized performance-based exam objectives and aligned with individual and team games, the NCL is a first-of-its-kind ongoing experiment in learning and gaming using next-generation high-fidelity simulation environments.</p>
+											<footer>
+												<ul class="actions">
+													<li><a href="http://www.nationalcyberleague.org" class="button">Learn More</a></li>
+												</ul>
+											</footer>
+										</section>
+										<hr />
+										<section>
+											<a href="http://www.maccdc.org" class="image fit"><img src="images/maccdc.jpg" alt="" /></a>
+											<h3>Mid-Atlantic Collegiate Cyber Defense Competition</h3>
+											<p>The Mid-Atlantic Collegiate Cyber Defense Competition started in 2006. MACCDC is a "inherit and defend" competition. In a nutshell, a group of 8 students are asked to take over a pre configured network. The network often simulates common business configurations in design and aims to be as life like as possible. When the students inherit the network, they then have to fend off waves off hackers known as the Red Team. Additionally, while all of this is going on, injects are given, which basically come down to basic business task that must be completed, all of which are time sensitive.</p>
+											<footer>
+												<ul class="actions">
+													<li><a href="http://maccdc.org" class="button">Learn More</a></li>
+												</ul>
+											</footer>
+										</section>
+									</section>
+
+							</div>
+							<div class="8u$ 12u$(medium) important(medium)">
+
+								<!-- Content -->
+									<section id="content">
+										<h2><strong>How We Did</strong></h2>
+										<h3>National Cyber League</h3>
+										<ul>
+											<li><strong>2013:</strong> WVU finished 4th in the eastern conference and 11th nationally. The WVU team consisted of all undergraduates ranging from freshman to senior. Of the 16 WVU students that competed, 7 broke the top 100 student in the nationwide-silver bracket for individuals. (Connor Anderson, Alan Fluder, Kyle Frank,Barry Martin, Zachary Prithchard, Mark Swisher, and Guytano Vignoli). Additionally, Barry Martin ranked 1st nationally in the silver bracket. </li>
+											<li><strong>2012:</strong> We were 10th in our conference The Spring 2012 Season of NCL. We also had a strong performance in the Fall 2012 NCL Season, with multiple team members in the top 25% of competitors</li>
+										</ul>
+										<h3>Mid-Atlantic Collegiate Cyber Defense Competition</h3>
+										<ul>
+											<li><strong>2014:</strong> WVU placed in the top 3 in the MACCDC virtual qualifer among 30+ schools. WVU then competed against 7 others at the 2014 MACCDC Regional Competition. </li>
+											<li><strong>2013:</strong> 2012 was the first year WVU took part in this competition. WVU took 2nd in their respected qualifying round division and 7th in the 2012 Mid-Atlantic Regional Finals at Johns Hopkins University Applied Physics Lab. </li>
+										</ul>
+									</section>
+
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+						<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+						<li><a href="#" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
